bd8934e8c180f14d5b2c2ee3a12e26c7e672b500
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2021-04-12 20:28:04 +0300

    MDropDownsetup

---
+++ src/lib-components/MDropDown.vue
@@ -0,0 +1,13 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default /*#__PURE__*/defineComponent({
+  name: 'MDropDown', // vue component name
+});
+</script>
+
+<template>
+  
+</template>
+
+
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export { default as Mega3UiSample } from './mega3-ui-sample.vue';
+export { default as MDropDown } from './MDropDown.vue';
